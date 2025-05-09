209fe82d41f752b9ed1899e9f3603d8af3a9e9bf
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-05-09 21:08:45 +0200

    feat: board init & basic loop

---
+++ init.c
@@ -0,0 +1,24 @@
+#include "2048.h"
+
+void init_board(t_board *board)
+{
+	srand(time(NULL));
+	for (int a = 0; a < 5; a++)
+	{
+		for (int b = 0; b < 5; b++)
+		{
+			board->tiles[a][b].number = 0;
+			board->tiles[a][b].merged = false;
+		}
+	}
+	if (board->size == 5)
+		board->empty_tiles = 25;
+	else
+	{
+		board->empty_tiles = 16;
+		for (int a = 0; a < 5; a++)
+			board->tiles[a][4].number = -1;
+		for (int b = 0; b < 5; b++)
+			board->tiles[4][b].number = -1;
+	}
+}
