e68a16039c2079233b8614b9086f0aa6c0dd57fe
icseri <icseri@student.42vienna.com>
2025-05-09 19:54:43 +0200

    🔧 chore: set up project and write TODOs

---
+++ 2048.h
@@ -0,0 +1,6 @@
+#pragma once
+
+enum e_const
+{
+	WIN_VALUE = 2048
+};
---
+++ main.c
@@ -0,0 +1,26 @@
+#include "2048.h"
+
+int main(void)
+{
+	//TODO: check WIN_VALUE " This value is taken into account only if it is a power of 2"
+	//TODO: setup ncurses
+	//TODO: keyhook for ESC
+	//TODO: prompt for grid size
+		//buttons 4 x 4 or 5 x 5
+	//TODO: draw grid
+		//print instructions
+		//draw windows (tiles)
+	//TODO: spawn first 2 numbers
+	//TODO: keyhook for arrow keys
+		//game logic
+	//game start
+	//TODO: spawn random empty tile, 2 -> 90%, 4 -> 10%
+
+	//TODO: write best score to file
+
+	//TODO: color tiles
+	//TODO: ascii art for numbers
+
+	//TODO: scores shown on the side
+
+}
