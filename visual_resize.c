8f477dd8484187b8a789d57194df75d684ca1a9a
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-05-11 21:38:33 +0200

    refactor: color macros

--- visual_resize.c
+++ visual_resize.c
@@ -28,7 +28,7 @@ static t_result window_too_small_loop(t_board *board)
 		keypad(stdscr, TRUE);
 		erase();
 		refresh();
-		wbkgd(stdscr, COLOR_PAIR(20));
+		wbkgd(stdscr, COLOR_PAIR(COLOR_TOOSMALL_WINDOW));
 		wattron(stdscr, A_BOLD);
 		unsigned int x = board->screen_x / 2 - 18 / 2;
 		unsigned int y = board->screen_y / 2 - 1;
@@ -40,11 +40,11 @@ static t_result window_too_small_loop(t_board *board)
 		len += numlen(board->min_screen_y);
 		x = board->screen_x / 2 - (len + 17) / 2;
 		y += 2;
-		attron(COLOR_PAIR(21));
+		attron(COLOR_PAIR(COLOR_TOOSMALL_TEXT));
 		mvprintw(y, x,"size (%i,%i) need (%i,%i)", // 17 + len
 			board->screen_x, board->screen_y,
 			board->min_screen_x, board->min_screen_y);
-		attroff(COLOR_PAIR(22));
+		attroff(COLOR_PAIR(COLOR_TOOSMALL_TEXT));
 		if (getch() == ESCAPE)
 			return (USER_EXIT);
 		getmaxyx(stdscr, board->screen_y, board->screen_x);
