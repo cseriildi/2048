382bacf0f3655404df7937e9a7ace5f53f7b9735
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-05-11 15:40:06 +0200

    fix:  background glitch from too small

--- visual_resize.c
+++ visual_resize.c
@@ -28,7 +28,7 @@ static t_result window_too_small_loop(t_board *board)
 {
 	while (board->screen_x < board->min_screen_x ||
 		   board->screen_y < board->min_screen_y)
-	{ // BUG: background text glitch when resizing from minimum size + 1
+	{
 		keypad(stdscr, TRUE);
 		mvprintw(ROW_TITLE,COL_TEXT,"terminal too small");
 		mvprintw(ROW_TITLE + 1,COL_TEXT,"size (%i,%i) need (%i,%i)",
@@ -46,9 +46,16 @@ static t_result window_too_small_loop(t_board *board)
 // t_result	window_resize_loop(t_board *board)
 t_result	window_size_check(t_board *board)
 {
+	t_result res;
 	getmaxyx(stdscr, board->screen_y, board->screen_x);
 	unsigned int min_board_x = MAX_BOARD_SIZE * (MIN_TILE_X + MIN_TILE_SPACING * 2);
 	board->min_screen_x = min_board_x + MIN_SCORE_X + WINDOW_SPACING;
 	board->min_screen_y = MAX_BOARD_SIZE * (MIN_TILE_Y + MIN_TILE_SPACING) + 1;
-	return window_too_small_loop(board);
+	res = window_too_small_loop(board);
+	if (res == SUCCESS)
+	{
+		erase();
+		refresh();
+	}
+	return res;
 }
